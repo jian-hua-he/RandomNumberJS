b9f20bdfe1b6d757a054b167296c606fa457f816
Jian-Hua, He <game09116@gmail.com>
2017-09-26 22:36:54 +0800

    Update style

--- index.js
+++ index.js
@@ -10,26 +10,26 @@
 
 })(function (window, $, elements) {
 
-    var stop = true;
-    var interval;
+    // var stop = true;
+    // var interval;
 
-    elements.btn.on('click', function () {
-        stop = !stop;
+    // elements.btn.on('click', function () {
+    //     stop = !stop;
 
-        if (stop) {
-            elements.btn.text('START');
-            window.clearInterval(interval);
-        } else {
-            var max = parseInt($('#max').val());
-            var min = parseInt($('#min').val());
+    //     if (stop) {
+    //         elements.btn.text('START');
+    //         window.clearInterval(interval);
+    //     } else {
+    //         var max = parseInt($('#max').val());
+    //         var min = parseInt($('#min').val());
 
-            elements.btn.text('STOP');
-            interval = window.setInterval(function() {
-                var randomNum = getRandomInt(min, max);
-                elements.randomNum.text(randomNum);
-            }, 50);
-        }
-    });
+    //         elements.btn.text('STOP');
+    //         interval = window.setInterval(function() {
+    //             var randomNum = getRandomInt(min, max);
+    //             elements.randomNum.text(randomNum);
+    //         }, 50);
+    //     }
+    // });
 
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
